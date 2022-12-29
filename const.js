2b0b59bf5bab8cf96af8d0022a2bf3388582490a
My Roman <klaytfrg@gmail.com>
2022-12-29 13:30:40 +0200

    first commit

---
+++ const.js
@@ -0,0 +1,33 @@
+const commands = `
+/start - Перезавантажити бота
+/help - допомога
+`
+const sait = `
+Відвідайте наш сайт: <a href="envaki.github.io">Сайт</a>
+`
+const onas = `
+Комунальне підприємство «Центр первинної медико-санітарної допомоги» Житомирської міської ради створено рішенням Житомирської міської ради від 18.12.2017 №855 для надання первинної медичної допомоги.
+
+Підприємство надає медичні послуги пацієнтам, якими подано декларації про вибір лікаря, що надає первинну медичну допомогу, лікарям підприємства.
+
+Первинна медична допомога надається Центром відповідно до Порядку надання первинної медичної допомоги, затвердженого наказом Міністерства охорони здоров’я України від 19.03.2018 №504.
+
+У Центрі працюють 131 лікарів загальної практики – сімейної медицини та 63 педіатрів.
+Більш детально за посиланням: <a href="https://likar.center/pro-nas/">Про нас</a>
+`
+const contacts = `\u{1F3E5}: майдан Визволення, буд. 1, м. Житомир, Житомирська область, 10020
+\u{1F4E7}: mail@likar.center
+\u{260E}: 0 (800) 35 04 36
+`
+const zapus = `
+<a href="https://portal-doctor.eleks.com/web/ml2zhytomyr/registration.html">Запис на прийом</a>
+`
+const button = `
+<a href="https://www.youtube.com/watch?v=YrsJPTMMzss&ab_channel=KHARKIVToday">Ти кнопка</a> 
+`
+module.exports.commands = commands
+module.exports.sait = sait
+module.exports.onas = onas
+module.exports.zapus = zapus
+module.exports.contacts = contacts
+module.exports.button = button
---
+++ index.js
@@ -0,0 +1,158 @@
+const { Telegraf, Markup } = require('telegraf');
+require('dotenv').config()
+const text = require('./const');
+
+const bot = new Telegraf('5869058937:AAH3kWPtduDnGYMHn6seheAUA4byi1UvWm4');
+// const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.command('start', ctx => {
+   bot.telegram.sendMessage(ctx.chat.id, '\u{1F3E3}Головне меню',
+    { 
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text:'\u{2139}Про нас', callback_data: 'about'},
+                    {text:' \u{260E}  Сайт', parse_mod:"HTML", callback_data: 'sait'}  
+                ],
+                [
+                    {text:'\u{1F404}Контакти', callback_data: 'contacts'},
+                    {text:'\u{1F4DD} Запис на прийом', callback_data: 'zapus'}  
+                ],
+                [
+                    {text:'\u{1F608}Просто кнопка', callback_data: 'button'}
+                ]
+            ]
+        }
+    })
+})
+
+function btnAdder(name, text) {
+    bot.action(name, async(ctx)=> {
+        try {
+            await ctx.answerCbQuery();
+            await  ctx.replyWithHTML(text)
+            await  bot.telegram.sendMessage(ctx.chat.id, 'Повернутися',
+            { 
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            {text:'До головного меню', callback_data: 'menu'},
+                        ]
+                    ]
+                }
+        })
+        } catch(e) {
+            console.error(e)
+        }
+    })
+}
+
+btnAdder('sait', text.sait)
+btnAdder('contacts', text.contacts)
+btnAdder('zapus', text.zapus)
+btnAdder('button', text.button)
+
+
+
+
+
+bot.action('about', async ctx => {
+    try {
+    await ctx.deleteMessage();
+    await  ctx.replyWithHTML(text.onas)
+    await bot.telegram.sendMessage(ctx.chat.id, 'Повернутися',
+    { 
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text:'До головного меню', callback_data: 'menu'},
+                ]
+            ]
+        }
+})
+    } catch (e) {
+        console.error(e)
+    }
+})
+bot.action('menu', async ctx => {
+    try {
+        await ctx.deleteMessage();
+        await bot.telegram.sendMessage(ctx.chat.id, '\u{1F3E3}Головне меню',
+        { 
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {text:'\u{2139}Про нас', callback_data: 'about'},
+                        {text:' \u{260E}  Сайт', callback_data: 'sait'}  
+                    ],
+                    [
+                        {text:'\u{1F404}Контакти', callback_data: 'contacts'},
+                        {text:'\u{1F4DD} Запис на прийом', callback_data: 'zapus'}  
+                    ],
+                    [
+                        {text:'\u{1F608}Просто кнопка', callback_data: 'button'}
+                    ]
+                ]
+            }
+    })
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+
+
+/*
+bot.start((ctx) => ctx.replyWithHTML('Вас вітає БОТ Житомирського ЦПМСД', Markup.inlineKeyboard([
+    [Markup.button.callback('Про нас', 'btn_1'), Markup.button.callback('Стікер', 'btn_2')],
+    [Markup.button.callback('Сайт', 'btn_3'), Markup.button.callback('Запис на прийом', 'btn_4')]
+]))
+);
+bot.command('info', (ctx) => ctx.reply('команди бота: /start'));
+bot.help((ctx) => ctx.reply(text.commands));
+
+bot.action('back',(ctx) => ctx.sendMessage('Вас вітає БОТ Житомирського ЦПМСД', Markup.inlineKeyboard([
+    [Markup.button.callback('Про нас', 'btn_1'), Markup.button.callback('Стікер', 'btn_2')],
+    [Markup.button.callback('Сайт', 'btn_3'), Markup.button.callback('Запис на прийом', 'btn_4')]
+])));
+function btnAdder(name, text) {
+    bot.action(name, async(ctx)=> {
+        try {
+            await ctx.answerCbQuery();
+            await  ctx.replyWithHTML(text)
+            await  bot.telegram.replyWithHTML(ctx.chat.id, {
+                reply_markup: {
+                    inline_keyboard: [
+                        {text:'Повернутись до меню', callback_data: 'back'}
+                    ]
+                }
+            })
+        } catch(e) {
+            console.error(e)
+        }
+    })
+}
+
+btnAdder('btn_1', text.onas)
+btnAdder('btn_3', text.text)
+btnAdder('btn_4', text.zapus)
+
+
+bot.action('btn_2', async(ctx)=> {
+    try {
+        await ctx.answerCbQuery();
+      await  ctx.reply('Send me a sticker')
+      await bot.on('sticker', (ctx) => ctx.reply('👍'));
+       disable_web_page_preview: true
+    } catch(e) {
+        console.error(e)
+    }
+})
+*/
+
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
